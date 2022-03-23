0051ac9fe796bfb850e17b78c800603168111725
Filipengbergs <filip792@hotmail.com>
2022-03-23 11:59:50 +0100

    Implemented listener and HBS setup

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-require("dotenv").config();
-const moongose = require("moongose");
-const express = require("express");
+require("dotenv/config");
+require("./mongoose.js");
+
 const exphbs = require("express-handlebars");
+
+const express = require("express");
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(8000, () => {
+  console.log("http://localhost:8000/");
+});
